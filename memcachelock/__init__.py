0febd8fbc4c2d30404c300b7d60b2a08939f6b66
Vincent Pelletier <plr.vincent@gmail.com>
2011-10-14 00:06:33 +0200

    Memcached documentation on incr states it uses 64bits integers.
    python-memcache doc talks about 2**32, which seems to be an error.

--- memcachelock/__init__.py
+++ memcachelock/__init__.py
@@ -20,7 +20,7 @@ class MemcacheRLock(object):
     See thread.LockType documentation for API.
 
     How to break things:
-    - create a lock instance, then 2**32 others (trash them along the way, you
+    - create a lock instance, then 2**64 others (trash them along the way, you
       don't need to keep them). Next one will think it is the first instance.
     - restart memcache server while one client has taken a lock (unles your
       memcache server restarts from persistent data properly written to disk).
